0e6be67ffc01176ef477dc7ab03a1e8e2ac31d25
Lauren Gibbs <lauren@chartiq.com>
2017-05-23 08:43:10 -0400

    font options implemented

--- src/components/DrawingToolbar.jsx
+++ src/components/DrawingToolbar.jsx
@@ -22,17 +22,10 @@ class DrawingToolbar extends React.Component {
 			// Sync the defaults for font tool
 			var style = this.props.ciq.canvasStyle("stx_annotation");
 
-			var initialSize = this.props.ciq.currentVectorParameters.annotation.font.size;
-			if (!initialSize){
-				initialSize = style.fontSize;
-			}
-			this.props.ciq.currentVectorParameters.annotation.font.size=CIQ.stripPX(initialSize);
-
-			var initialFamily = this.props.ciq.currentVectorParameters.annotation.font.family;
-			if (!initialFamily){
-				initialFamily = style.fontFamily;
-			}
-			this.props.ciq.currentVectorParameters.annotation.font.family=initialFamily;
+      this.props.ciq.currentVectorParameters.annotation.font.size=style.fontSize;
+      this.props.ciq.currentVectorParameters.annotation.font.family=style.fontFamily;
+      this.props.ciq.currentVectorParameters.annotation.font.style=style.fontStyle;
+      this.props.ciq.currentVectorParameters.annotation.font.weight=style.fontWeight;
 		}
 		// Set all the info for the toolbar
 		this.setState({
@@ -72,10 +65,16 @@ class DrawingParameters extends React.Component {
 			lineWidth:null,
 			linePattern:null,
 			parameters:null,
-			fontOptions:null
-		}
+			fontOptions:null,
+      fontFamily:null,
+      fontSize:null
+		};
 		this.openColorPicker = this.openColorPicker.bind(this);
 		this.updateLineParams = this.updateLineParams.bind(this);
+		this.updateFontFamily = this.updateFontFamily.bind(this);
+    this.updateFontSize = this.updateFontSize.bind(this);
+    this.toggleBold = this.toggleBold.bind(this);
+    this.toggleItalic = this.toggleItalic.bind(this);
 	}
 	componentWillReceiveProps(nextProps) {
 		if (nextProps.parameters) {
@@ -84,7 +83,10 @@ class DrawingParameters extends React.Component {
 				line:nextProps.parameters.color,
 				lineWidth:nextProps.parameters.lineWidth,
 				linePattern:nextProps.parameters.pattern,
-				parameters: nextProps.parameters
+				parameters: nextProps.parameters,
+        fontOptions: nextProps.parameters.font,
+        fontFamily: nextProps.parameters.font.family,
+        fontSize: nextProps.parameters.font.size
 			});
 		}
 	}
@@ -111,6 +113,24 @@ class DrawingParameters extends React.Component {
 		this.props.ciq.currentVectorParameters.lineWidth=weight;
 		this.props.ciq.currentVectorParameters.pattern=pattern;
 	}
+	updateFontFamily(newFamily){
+	  this.props.ciq.changeVectorParameter("fontFamily", newFamily);
+  }
+  updateFontSize(newSize){
+    this.props.ciq.changeVectorParameter("fontSize", newSize+'px');
+  }
+  toggleItalic(bool){
+    if(bool)
+      this.props.ciq.changeVectorParameter("fontStyle", "italic");
+    else
+      this.props.ciq.changeVectorParameter("fontStyle", "normal");
+  }
+  toggleBold(bool){
+    if(bool)
+      this.props.ciq.changeVectorParameter("fontWeight", "bold");
+    else
+      this.props.ciq.changeVectorParameter("fontWeight", "normal");
+  }
 	render() {
 		if(!this.state.parameters) return <span></span>;
 		return(
@@ -120,10 +140,10 @@ class DrawingParameters extends React.Component {
 					<FillColor color={this.state.fill} openColorPicker={this.openColorPicker} />
 					<LineColor color={this.state.line} openColorPicker={this.openColorPicker} />
 					<LineStyle width={this.state.lineWidth} pattern={this.state.linePattern} updateLineParams={this.updateLineParams} />
-					<Bold fontOptions={this.state.fontOptions} />
-					<Italic fontOptions={this.state.fontOptions} />
-					<FontSize fontOptions={this.state.fontOptions} />
-					<FontFamily fontOptions={this.state.fontOptions} />
+					<Bold fontOptions={this.state.fontOptions} toggleBold={this.toggleBold} />
+					<Italic fontOptions={this.state.fontOptions} toggleItalic={this.toggleItalic} />
+					<FontSize fontOptions={this.state.fontOptions} size={this.state.fontSize} updateFontSize={this.updateFontSize} />
+					<FontFamily fontOptions={this.state.fontOptions} family={this.state.fontFamily} updateFontFamily={this.updateFontFamily} />
 				</div>
 			</span>
 		)
@@ -172,7 +192,7 @@ class LineStyle extends React.Component {
 		super(props);
 		this.state = {
 			selectedLineClass:"ciq-"+this.props.pattern+"-"+this.props.width,
-		}
+		};
 		this.onClick = this.onClick.bind(this);
 	}
 	onClick(newClass, newWeight, newPattern) {
@@ -207,53 +227,130 @@ class LineStyle extends React.Component {
 	}
 }
 
-var Bold = React.createClass({
-	onClick(e){
-		this.props.openColorPicker(e.target);
-	},
-	render: function(){
+class Bold extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.state={
+      bold:false
+    };
+  }
+	onClick() {
+    var self=this;
+    return function(){
+      self.setState({
+        bold:!self.state.bold
+      }, () => {
+        self.props.toggleBold(self.state.bold);
+      });
+    }
+	}
+	render() {
 		if(!this.props.fontOptions) return <span></span>;
 		return(
-			<span><div className="boldBtn" onClick={this.onClick()} style="font-weight:bold">B</div></span>
+			<span><div className="boldBtn" onClick={this.onClick()}>B</div></span>
 		)
 	}
-});
+}
 
-var Italic = React.createClass({
-	onClick(e){
-		this.props.openColorPicker(e.target);
-	},
-	render: function(){
+class Italic extends React.Component{
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.state={
+      italic:false
+    };
+  }
+	onClick() {
+    var self=this;
+    return function(){
+      self.setState({
+        italic:!self.state.italic
+      }, () => {
+        self.props.toggleItalic(self.state.italic);
+      });
+    };
+	}
+	render() {
 		if(!this.props.fontOptions) return <span></span>;
 		return(
-			<span><div className="boldBtn" onClick={this.onClick()} style="font-weight:bold">B</div></span>
+			<span><div className="italicBtn" onClick={this.onClick()}>I</div></span>
 		)
 	}
-});
+}
 
-var FontSize = React.createClass({
-	onClick(e){
-		this.props.openColorPicker(e.target);
-	},
-	render: function(){
+class FontSize extends React.Component{
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.fontSizeOptions=[8, 10, 12, 13, 14, 16, 20, 28, 36, 48, 64];
+    this.state={
+      fontSize:this.props.size
+    };
+  }
+	onClick(newSize) {
+    var self=this;
+    return function(){
+      self.setState({
+        fontSize:newSize+'px',
+      });
+      self.props.updateFontSize(newSize);
+    }
+	}
+	render() {
 		if(!this.props.fontOptions) return <span></span>;
-		return(
-			<span><div className="boldBtn" onClick={this.onClick()} style="font-weight:bold">B</div></span>
-		)
+    var self=this;
+    var fontSizes = this.fontSizeOptions.map(function(option, index) {
+      return <menu-option key={"size" + index} onClick={self.onClick(option)}><span>{option}</span></menu-option>
+    });
+    return(
+      <span>
+        <menu-select id="fontSizeSelect">
+				<span className="title">{this.state.fontSize}</span>
+				<menu-select-options class="menu-hover">
+          {fontSizes}
+				</menu-select-options>
+			</menu-select>
+      </span>
+    )
 	}
-});
+}
 
-var FontFamily = React.createClass({
-	onClick(e){
-		this.props.openColorPicker(e.target);
-	},
-	render: function(){
+class FontFamily extends React.Component{
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.fontFamilyOptions=["Helvetica", "Courier", "Garamond", "Palatino", "Times New Roman"];
+    this.state={
+      fontFamily:this.props.family
+    };
+  }
+	onClick(newFamily) {
+    var self=this;
+    return function(){
+      self.setState({
+        fontFamily:newFamily
+      });
+      self.props.updateFontFamily(newFamily);
+    }
+	}
+	render() {
 		if(!this.props.fontOptions) return <span></span>;
+		var self=this;
+    var fontFamilies = this.fontFamilyOptions.map(function(option, index) {
+      return <menu-option key={"family" + index} onClick={self.onClick(option)}><span>{option}</span></menu-option>
+    });
 		return(
-			<span><div className="boldBtn" onClick={this.onClick()} style="font-weight:bold">B</div></span>
+			<span>
+        <menu-select id="fontFamilySelect">
+				<span className="title">{this.state.fontFamily}</span>
+				<menu-select-options class="menu-hover">
+          {fontFamilies}
+				</menu-select-options>
+			</menu-select>
+      </span>
 		)
 	}
-});
-
+}
 
 module.exports = DrawingToolbar;
