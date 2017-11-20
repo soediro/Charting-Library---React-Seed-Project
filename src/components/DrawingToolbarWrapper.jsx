c321a0d81f21c1482e5f2ba2f40f12637d2b0398
Brad Carter <carterbs89@gmail.com>
2017-11-20 13:39:11 -0500

    Missed drawingToolbar

--- src/components/DrawingToolbarWrapper.jsx
+++ src/components/DrawingToolbarWrapper.jsx
@@ -1,5 +1,5 @@
 import { ChartStore, Actions } from "../stores/ChartStore";
-
+import DrawingToolbar from './DrawingToolbar';
 export default class DrawingToolbarWrapper extends React.Component {
 	constructor(props) {
 		super(props);
