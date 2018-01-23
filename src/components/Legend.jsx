51c6a7182cf40b8e2f7f1ddb365140e8ceaa0f9f
Ian German Mesner <ian@chartiq.com>
2018-01-23 11:09:15 -0500

    5523 comparison; don't initiate action to remove comparison, since that is handled in the callback anyway

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -1,15 +1,18 @@
 const Legend = (props) => {
 
-  console.log(props.comparisons)
-
   if (props.comparisons && props.comparisons.length === 0) return (<span></span>)
+
+  var removeComparison = function(comparison){
+    // action is handled via callback in in Comparison.jsx, so just remove the series to initiate
+    props.ciq.removeSeries(comparison.id)
+  }
 
 	let comparisons = props.comparisons.map((comparison, i) => {
 		return (
 			<div className="comparisonWrapper" key={"comparison" + i}>
 				<div className="chartSeriesColor" style={{ 'backgroundColor': comparison.parameters.color }}></div>
 				<div className="chartSeries">{comparison.display}</div>
-				<div className="deleteSeries" onClick={props.removeComparison.bind(this, comparison)}></div>
+				<div className="deleteSeries" onClick={()=>removeComparison(comparison)}></div>
 			</div>
 		)
 	})
