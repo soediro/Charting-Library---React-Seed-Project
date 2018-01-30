783152b10771c093090730117ef5c20701fe8679
Joseph Claus <joseph@chartiq.com>
2018-01-30 12:08:32 -0500

    Changes to fix problems

--- src/components/Drawing/LineStyle.jsx
+++ src/components/Drawing/LineStyle.jsx
@@ -1,28 +1,55 @@
-const LineStyle = (props) => {
-	let cName = props.linePattern && props.lineWidth ? 'ciq-' + props.linePattern + '-' + props.lineWidth : null;
-
-	if(cName){
-		return (
-			<span>
-				<menu-select id='lineSelect'>
-				<span className={'title ' + cName}></span>
-					<menu-select-options className="menu-hover">
-						<menu-option class="option" onClick={props.onClick.bind(this, 1, 'solid')}><span className="ciq-line-style-option ciq-solid-1"></span></menu-option>
-						<menu-option class="option" onClick={props.onClick.bind(this, 3, 'solid')}><span className="ciq-line-style-option ciq-solid-3"></span></menu-option>
-						<menu-option class="option" onClick={props.onClick.bind(this, 5, 'solid')}><span className="ciq-line-style-option ciq-solid-5"></span></menu-option>
-						<menu-option class="option" onClick={props.onClick.bind(this, 1, 'dotted')}><span className="ciq-line-style-option ciq-dotted-1"></span></menu-option>
-						<menu-option class="option" onClick={props.onClick.bind(this, 3, 'dotted')}><span className="ciq-line-style-option ciq-dotted-3"></span></menu-option>
-						<menu-option class="option" onClick={props.onClick.bind(this, 5, 'dotted')}><span className="ciq-line-style-option ciq-dotted-5"></span></menu-option>
-						<menu-option class="option" onClick={props.onClick.bind(this, 1, 'dashed')}><span className="ciq-line-style-option ciq-dashed-1"></span></menu-option>
-						<menu-option class="option" onClick={props.onClick.bind(this, 3, 'dashed')}><span className="ciq-line-style-option ciq-dashed-3"></span></menu-option>
-						<menu-option class="option" onClick={props.onClick.bind(this, 5, 'dashed')}><span className="ciq-line-style-option ciq-dashed-5"></span></menu-option>
-					</menu-select-options>
-				</menu-select>
-			</span>
-		)
-	} else {
-		return (<span></span>)
+class LineStyle extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			menuOpen: false
+		}
+		this.openMenu = this.openMenu.bind(this);
+		this.closeMenu = this.closeMenu.bind(this);
 	}
+	openMenu(){
+		this.setState({
+			menuOpen: true
+		});
+	}
+	closeMenu(){
+		this.setState({
+			menuOpen: false
+		});
+	}
+	render(){
+		let cName = this.props.linePattern && this.props.lineWidth ? 'ciq-' + this.props.linePattern + '-' + this.props.lineWidth : null;
+
+		let menuDisplay = {
+			display: this.state.menuOpen ? 'block' : 'none'
+		};
+
+		if(cName){
+			return (
+				<span>
+					<menu-select id='lineSelect' onMouseOver={this.openMenu} onMouseOut={this.closeMenu} onClick={this.closeMenu}>
+						<span className={'title ' + cName}></span>
+						<menu-select-options className="menu-hover" style={menuDisplay}>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 1, 'solid')}><span className="ciq-line-style-option ciq-solid-1"></span></menu-option>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 3, 'solid')}><span className="ciq-line-style-option ciq-solid-3"></span></menu-option>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 5, 'solid')}><span className="ciq-line-style-option ciq-solid-5"></span></menu-option>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 1, 'dotted')}><span className="ciq-line-style-option ciq-dotted-1"></span></menu-option>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 3, 'dotted')}><span className="ciq-line-style-option ciq-dotted-3"></span></menu-option>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 5, 'dotted')}><span className="ciq-line-style-option ciq-dotted-5"></span></menu-option>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 1, 'dashed')}><span className="ciq-line-style-option ciq-dashed-1"></span></menu-option>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 3, 'dashed')}><span className="ciq-line-style-option ciq-dashed-3"></span></menu-option>
+							<menu-option class="option" onClick={this.props.onClick.bind(this, 5, 'dashed')}><span className="ciq-line-style-option ciq-dashed-5"></span></menu-option>
+						</menu-select-options>
+					</menu-select>
+				</span>
+			)
+		}else{
+			return (
+				<span></span>
+			)
+		}
+	}
+
 }
 
 export default LineStyle
