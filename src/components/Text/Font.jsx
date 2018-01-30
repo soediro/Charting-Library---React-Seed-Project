28b0204bfa43c432e7bb11313686e6cdcda36f2c
Joseph Claus <joseph@chartiq.com>
2018-01-30 13:17:12 -0500

    Fixes popup dropdown menus for font

--- src/components/Text/Font.jsx
+++ src/components/Text/Font.jsx
@@ -1,34 +1,61 @@
-const families = ["Helvetica", "Courier", "Garamond", "Palatino", "Times New Roman"]
-const sizes = [8, 10, 12, 13, 14, 16, 20, 28, 36, 48, 64]
+class Font extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            sizeMenuOpen: false,
+            familyMenuOpen: false,
+            sizes: [8, 10, 12, 13, 14, 16, 20, 28, 36, 48, 64],
+            families: ["Helvetica", "Courier", "Garamond", "Palatino", "Times New Roman"]
+        };
+        console.log('this.state.sizes: ', this.state.sizes);
+        console.log('this.state.familes: ', this.state.families)
+        this.setSizeMenu = this.setSizeMenu.bind(this);
+        this.setFamilyMenu = this.setFamilyMenu.bind(this);
+    }
+    setSizeMenu(open){
+        this.setState({ sizeMenuOpen: open })
+    }
+    setFamilyMenu(open){
+        this.setState({ familyMenuOpen: open })
+    }
+    render(){
+        if (!this.props.fontOptions) { return (<span></span>) }
+        console.log('this.state: ', this.state)
 
-const Font = (props) => {
-    if (!props.fontOptions) return (<span></span>)
+        let familyOptions = this.state.families.map((family, i) => {
+            return (<menu-option key={'family' + i} onClick={this.props.onFamilyClick.bind(this, family)}><span>{family}</span></menu-option>)
+        })
 
-    let familyOptions = families.map((family, i) => {
-        return (<menu-option key={'family' + i} onClick={props.onFamilyClick.bind(this, family)}><span>{family}</span></menu-option>)
-    })
+        let sizeOptions = this.state.sizes.map((size, i) => {
+            return (<menu-option key={'size' + i} onClick={this.props.onSizeClick.bind(this, size)}><span>{size}</span></menu-option>)
+        })
 
-    let sizeOptions = sizes.map((size, i) => {
-        return (<menu-option key={'size' + i} onClick={props.onSizeClick.bind(this, size)}><span>{size}</span></menu-option>)
-    })
+        let sizeMenuDisplay = {
+            display: this.state.sizeMenuOpen ? 'block' : 'none'
+        };
 
-    return (
-        <span>
-            <menu-select id='fontSizeSelect'>
-                <span className="title">{props.fontSize}</span>
-                <menu-select-options className="menu-hover">
-                    {sizeOptions}
-                </menu-select-options>
-            </menu-select>
+        let familyMenuDisplay = {
+            display: this.state.familyMenuOpen ? 'block' : 'none'
+        };
 
-            <menu-select id='fontFamilySelect'>
-                <span className="title">{props.fontFamily}</span>
-                <menu-select-options className="menu-hover">
-                    {familyOptions}
-                </menu-select-options>
-            </menu-select>
-        </span>
-    )
+        return (
+            <span>
+                <menu-select id='fontSizeSelect' onMouseOver={this.setSizeMenu.bind(this, true)} onMouseOut={this.setSizeMenu.bind(this, false)} onClick={this.setSizeMenu.bind(this, false)}>
+                    <span className="title">{this.props.fontSize}</span>
+                    <menu-select-options className="menu-hover" style={sizeMenuDisplay}>
+                        {sizeOptions}
+                    </menu-select-options>
+                </menu-select>
+
+                <menu-select id='fontFamilySelect' onMouseOver={this.setFamilyMenu.bind(this, true)} onMouseOut={this.setFamilyMenu.bind(this, false)} onClick={this.setFamilyMenu.bind(this, false)}>
+                    <span className="title">{this.props.fontFamily}</span>
+                    <menu-select-options className="menu-hover" style={familyMenuDisplay}>
+                        {familyOptions}
+                    </menu-select-options>
+                </menu-select>
+            </span>
+        )
+    }
 }
 
 export default Font
