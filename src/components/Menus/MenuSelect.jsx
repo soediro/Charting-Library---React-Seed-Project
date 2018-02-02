ad73f1dd7191dcac8fbdd4ae53cc06a772ebc01e
Joseph Claus <joseph@chartiq.com>
2018-02-02 16:28:28 -0500

    5732 Make menus open on click and close on mouse leave

--- src/components/Menus/MenuSelect.jsx
+++ src/components/Menus/MenuSelect.jsx
@@ -5,13 +5,26 @@ class MenuSelect extends React.Component{
             menuOpen: false
         }
         this.toggleMenu = this.toggleMenu.bind(this);
+        this.selectOption = this.selectOption.bind(this);
     }
     toggleMenu(open){
         this.setState({ menuOpen: open })
     }
+    selectOption(ciq, option){
+        this.setState({
+            menuOpen: false
+        }, () => {
+            if (Object.keys(ciq).length > 0){
+                this.props.handleOptionSelect(ciq, option);
+            } else {
+                this.props.handleOptionSelect(option);
+            }
+            
+        })
+    }
     render(){
         let options = this.props.options.map((option, i) => {
-            let onSelect = this.props.needsCiq ? this.props.handleOptionSelect.bind(this, this.props.ciq, option) : this.props.handleOptionSelect.bind(this, option),
+            let onSelect = this.props.needsCiq ? this.selectOption.bind(this, this.props.ciq, option) : this.selectOption.bind(this, {}, option),
             optionLabel = this.props.name ? option[this.props.name] : option
             return (
                 <menu-option key={'menuSelectOption' + this.props.keyName + i} className='option' onClick={onSelect}>{optionLabel}</menu-option>
@@ -23,7 +36,7 @@ class MenuSelect extends React.Component{
         };
 
         return (
-            <menu-select id={this.props.menuId} onMouseOver={this.toggleMenu.bind(this, true)} onMouseOut={this.toggleMenu.bind(this, false)} onClick={this.toggleMenu.bind(this, false)}>
+            <menu-select id={this.props.menuId} onMouseLeave={this.toggleMenu.bind(this, false)} onClick={this.toggleMenu.bind(this, !this.state.menuOpen)}>
                 <span className='title'>{this.props.title}</span>
                 <menu-select-options className='ps-container' style={menuDisplay}>
                     {options}
--- src/components/Menus/MenuSelectWithButtons.jsx
+++ src/components/Menus/MenuSelectWithButtons.jsx
@@ -9,6 +9,7 @@ class MenuSelect extends React.Component{
         this.toggleMenu = this.toggleMenu.bind(this);
         this.edit = this.edit.bind(this);
         this.delete = this.delete.bind(this);
+        this.selectOption = this.selectOption.bind(this);
     }
     toggleMenu(open){
         this.setState({ menuOpen: open })
@@ -21,11 +22,23 @@ class MenuSelect extends React.Component{
         event.stopPropagation();
         this.props.deleteItem(option);
     }
+    selectOption(ciq, option){
+        this.setState({
+            menuOpen: false
+        }, () => {
+            if (Object.keys(ciq).length > 0){
+                this.props.handleOptionSelect(ciq, option);
+            } else {
+                this.props.handleOptionSelect(option);
+            }
+        });
+    }
     render(){
         if(this.props.options.length===0) { return (<div></div>); }
 
         let options = this.props.options.map((option, i) => {
-            let onSelect = this.props.needsCiq ? this.props.handleOptionSelect.bind(this, this.props.ciq, option) : this.props.handleOptionSelect.bind(this, option),
+            // let onSelect = this.props.needsCiq ? this.props.handleOptionSelect.bind(this, this.props.ciq, option) : this.props.handleOptionSelect.bind(this, option),
+            let onSelect = this.props.needsCiq ? this.selectOption.bind(this, this.props.ciq, option) : this.selectOption.bind(this, option),
             optionLabel = this.props.name ? option[this.props.name] : option
 
             if(this.props.noButtons.indexOf(optionLabel)>-1){
@@ -50,7 +63,7 @@ class MenuSelect extends React.Component{
         };
 
         return (
-            <menu-select id={this.props.menuId} onMouseOver={this.toggleMenu.bind(this, true)} onMouseOut={this.toggleMenu.bind(this, false)} onClick={this.toggleMenu.bind(this, false)}>
+            <menu-select id={this.props.menuId} onMouseLeave={this.toggleMenu.bind(this, false)} onClick={this.toggleMenu.bind(this, !this.state.menuOpen)}>
                 <span className='title'>{this.props.title}</span>
                 <menu-select-options className='ps-container' style={menuDisplay}>
                     {options}
