f8a6b56e7fedbdc896eefebee2b21be0b8923bc5
Lauren Gibbs <lauren@chartiq.com>
2018-02-16 09:47:44 -0500

    5787 fix positioning of comparison legend

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -2,6 +2,15 @@ const Legend = (props) => {
 
   if (props.comparisons && props.comparisons.length === 0) return (<span></span>)
 
+	let position={marginTop:props.ciq.chart.top+35+'px'}
+	var reposition = function(){
+  	position={marginTop:props.ciq.chart.top+35+'px'}
+	}
+	props.ciq.callbacks.layout=function(){
+		console.log('in the layout callback')
+		reposition()
+	}
+
   var removeComparison = function(comparison){
     // action is handled via callback in in Comparison.jsx, so just remove the series to initiate
     props.ciq.removeSeries(comparison.id)
@@ -9,7 +18,7 @@ const Legend = (props) => {
 
 	let comparisons = props.comparisons.map((comparison, i) => {
 		return (
-			<div className="comparisonWrapper" key={"comparison" + i}>
+			<div className="comparisonWrapper" key={"comparison" + i} style={position}>
 				<div className="chartSeriesColor" style={{ 'backgroundColor': comparison.parameters.color }}></div>
 				<div className="chartSeries">{comparison.display}</div>
 				<div className="deleteSeries" onClick={()=>removeComparison(comparison)}></div>
