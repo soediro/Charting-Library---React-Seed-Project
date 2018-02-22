89b585dc5568c147a5de831004c2d547c8134bc1
Joseph Claus <joseph@chartiq.com>
2018-02-22 10:22:59 -0500

    Disable undo, redo, clear when necessary

--- src/components/Drawing/Redo.jsx
+++ src/components/Drawing/Redo.jsx
@@ -2,7 +2,7 @@ const Redo = (props) => {
 	let cName = props.canRedo ? 'ciq-btn active' : 'ciq-btn';
 
 	return (
-		<button className={cName} onClick={props.redo}>Redo</button>
+		<button disabled={!props.canRedo} className={cName} onClick={props.redo}>Redo</button>
 	);
 };
 
